c4a6daa2039970d01e2867cc8180f2e3bd17bff8
Wesley Hartford <github@cutterslade.ca>
2013-05-28 13:46:35 -0700

    Added a couple more unit tests

--- src/test/java/ca/cutterslade/util/jvmbuilder/sun/SimpleCreateJvmTest.java
+++ src/test/java/ca/cutterslade/util/jvmbuilder/sun/SimpleCreateJvmTest.java
@@ -1,11 +1,17 @@
 package ca.cutterslade.util.jvmbuilder.sun;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.util.Arrays;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import ca.cutterslade.util.jvmbuilder.ReaderThread;
+import ca.cutterslade.util.jvmbuilder.SizeUnit;
+
+import com.google.common.base.Joiner;
+import com.google.common.collect.ImmutableMap;
 
 public class SimpleCreateJvmTest {
 
@@ -17,10 +23,55 @@ public class SimpleCreateJvmTest {
     }
   }
 
+  public static final class MaxHeapMain {
+    public static void main(final String[] args) {
+      System.out.println(ManagementFactory.getMemoryMXBean().getHeapMemoryUsage().getMax());
+    }
+  }
+
+  public static final class PrintArgumentMain {
+    public static void main(String[] args) {
+      System.out.println(Joiner.on(' ').join(args));
+    }
+  }
+
+  public static final class PringPropertyMain {
+    public static void main(String[] args) {
+      System.out.println(System.getProperty("test.string"));
+    }
+  }
+
   @Test(timeout = 10_000)
   public void testStart() throws IOException, InterruptedException {
-    final SunJvmFactoryBuilder builder = new SunJvmFactoryBuilder();
-    builder.setMainClass(TestMain.class);
+    Assert.assertEquals(TEST_STRING + System.lineSeparator(), getStdOut(getBuilder(TestMain.class)));
+  }
+
+  @Test(timeout = 10_000)
+  public void testAdjustMaxHeap() throws InterruptedException, IOException {
+    final String stdOut = getStdOut(getBuilder(MaxHeapMain.class)
+        .setMaxHeapSpace(256, SizeUnit.MEGABYTES));
+    Assert.assertEquals("238616576" + System.lineSeparator(), stdOut);
+  }
+
+  @Test(timeout = 10_000)
+  public void testArgument() throws IOException, InterruptedException {
+    final String stdOut = getStdOut(getBuilder(PrintArgumentMain.class)
+        .setProgramArguments(Arrays.asList("hello", "world")));
+    Assert.assertEquals("hello world" + System.lineSeparator(), stdOut);
+  }
+
+  @Test(timeout = 10_000)
+  public void testProperty() throws IOException, InterruptedException {
+    final String stdOut = getStdOut(getBuilder(PringPropertyMain.class)
+        .setProperties(ImmutableMap.of("test.string", TEST_STRING)));
+    Assert.assertEquals(TEST_STRING + System.lineSeparator(), stdOut);
+  }
+
+  private static SunJvmFactoryBuilder getBuilder(final Class<?> mainClass) {
+    return new SunJvmFactoryBuilder().setMainClass(mainClass);
+  }
+
+  private static String getStdOut(final SunJvmFactoryBuilder builder) throws IOException, InterruptedException {
     final Process process = builder.start();
     final ReaderThread stdOut = new ReaderThread(process.getInputStream());
     stdOut.start();
@@ -32,7 +83,7 @@ public class SimpleCreateJvmTest {
     Assert.assertEquals(0, process.exitValue());
     Assert.assertEquals("", stdErr.getResult());
     Assert.assertNull(stdErr.getThrowable());
-    Assert.assertEquals(TEST_STRING + System.lineSeparator(), stdOut.getResult());
     Assert.assertNull(stdOut.getThrowable());
+    return stdOut.getResult();
   }
 }
